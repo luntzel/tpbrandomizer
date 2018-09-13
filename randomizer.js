b5ba1a94bddf0a27aa836cd609c29fe64e37b8f9
Mark Luntzel <mark@luntzel.com>
2018-09-12 18:39:24 -0700

    changed some variable names

--- randomizer.js
+++ randomizer.js
@@ -1,39 +1,39 @@
-var var1, var2, var3;
+var seasonSelection, numberOfEpisodes, episodeSelection;
 
 function getRandomArbitrary(min, max) {
   return Math.floor(Math.random() * (max - min) + min) + 1 ;
 }
 
-// calculate season
-var1 = getRandomArbitrary(1,12);
-document.getElementById("season").innerHTML = var1;
+// calculate seasonSelection
+seasonSelection = getRandomArbitrary(1,12);
+document.getElementById("season").innerHTML = seasonSelection;
 
-// calcuate episode based on season
-if (var1 = 1) {
-  var2 = 6
-} else if (var1 = 2) {
-  var2 = 7
-} else if (var1 = 3) {
-  var2 = 7
-} else if (var1 = 4) {
-  var2 = 8
-} else if (var1 = 5) {
-  var2 = 10
-} else if (var1 = 6) {
-  var2 = 6
-} else if (var1 = 7) {
-  var2 = 10
-} else if (var1 = 8) {
-  var2 = 10
-} else if (var1 = 9) {
-  var2 = 10
-} else if (var1 = 10) {
-  var2 = 10
-} else if (var1 = 11) {
-  var2 = 10
+// calcuate episodeSelection based on seasonSelection
+if (seasonSelection = 1) {
+  numberOfEpisodes = 6
+} else if (seasonSelection = 2) {
+  numberOfEpisodes = 7
+} else if (seasonSelection = 3) {
+  numberOfEpisodes = 7
+} else if (seasonSelection = 4) {
+  numberOfEpisodes = 8
+} else if (seasonSelection = 5) {
+  numberOfEpisodes = 10
+} else if (seasonSelection = 6) {
+  numberOfEpisodes = 6
+} else if (seasonSelection = 7) {
+  numberOfEpisodes = 10
+} else if (seasonSelection = 8) {
+  numberOfEpisodes = 10
+} else if (seasonSelection = 9) {
+  numberOfEpisodes = 10
+} else if (seasonSelection = 10) {
+  numberOfEpisodes = 10
+} else if (seasonSelection = 11) {
+  numberOfEpisodes = 10
 } else {
-  var2 = 10
+  numberOfEpisodes = 10
 }
 
-var3 = getRandomArbitrary(1, var2);
-document.getElementById("episode").innerHTML = var3;
+episodeSelection = getRandomArbitrary(1, numberOfEpisodes);
+document.getElementById("episode").innerHTML = episodeSelection;
