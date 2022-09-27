1c6f4740ad4df5a687e5a28e59390584894947f5
Mark Luntzel <mark_luntzel@mckinsey.com>
2022-09-27 14:57:34 -0700

    fixed Bach changes

--- public/randomizer.js
+++ public/randomizer.js
@@ -6,63 +6,74 @@ function getRandomArbitrary (min, max) {
 }
 
 // calculate seasonSelection
-seasonSelection = getRandomArbitrary(1, 12)
+// weight seasons 7 and 8 higher because Sebastian that greasy bastard
+var x = Math.floor(Math.random() * 100)
+//console.log("x= " + x)
+if (x <= 40) {
+    seasonSelection = 7
+} else if (x <= 80) {
+    seasonSelection = 8
+} else {
+    seasonSelection = getRandomArbitrary(0, 12)
+}
+
 document.getElementById('season').innerHTML = seasonSelection
 
 // calcuate episodeSelection based on seasonSelection
 if (seasonSelection === 1) {
   numberOfEpisodes = 6
+  var episodeSelection = getRandomArbitrary(1, numberOfEpisodes)
 } else if (seasonSelection === 2) {
   numberOfEpisodes = 7
+  var episodeSelection = getRandomArbitrary(1, numberOfEpisodes)
 } else if (seasonSelection === 3) {
   numberOfEpisodes = 7
+  var episodeSelection = getRandomArbitrary(1, numberOfEpisodes)
 } else if (seasonSelection === 4) {
   numberOfEpisodes = 8
+  var episodeSelection = getRandomArbitrary(1, numberOfEpisodes)
 } else if (seasonSelection === 5) {
   numberOfEpisodes = 10
+  var episodeSelection = getRandomArbitrary(1, numberOfEpisodes)
 } else if (seasonSelection === 6) {
   numberOfEpisodes = 6
+  var episodeSelection = getRandomArbitrary(1, numberOfEpisodes)
 } else if (seasonSelection === 7) {
   numberOfEpisodes = 10
-} else if (seasonSelection === 8) {
+  var y = Math.floor(Math.random() * 100)
+    if (y <= 25) {
+        var episodeSelection = 4
+        document.getElementById('bach').innerHTML = "SEBASTIAN BACH!"
+    } else if (y <= 50) {
+        var episodeSelection = 5
+        document.getElementById('bach').innerHTML = "SEBASTIAN BACH!"
+    } else if (y <= 75) {
+        var episodeSelection = 10
+        document.getElementById('bach').innerHTML = "SEBASTIAN BACH!"
+    } else {
+        var episodeSelection = getRandomArbitrary(1, numberOfEpisodes)
+    }
+  } else if (seasonSelection === 8) {
   numberOfEpisodes = 10
+  var z = Math.floor(Math.random() * 100)
+  if (z <= 50) {
+      var episodeSelection = 10
+      document.getElementById('bach').innerHTML = "SEBASTIAN BACH!"
+  } else {
+      var episodeSelection = getRandomArbitrary(1, numberOfEpisodes)
+  }
 } else if (seasonSelection === 9) {
   numberOfEpisodes = 10
+  var episodeSelection = getRandomArbitrary(1, numberOfEpisodes)
 } else if (seasonSelection === 10) {
   numberOfEpisodes = 10
+  var episodeSelection = getRandomArbitrary(1, numberOfEpisodes)
 } else if (seasonSelection === 11) {
   numberOfEpisodes = 10
+  var episodeSelection = getRandomArbitrary(1, numberOfEpisodes)
 } else {
-  numberOfEpisodes = 10
-}
-
-// weigh Sebastian Bach episodes higher
-if (seasonSelection === 7) {
-  var n = Math.floor(Math.random() * 100)
-  switch (n) {
-    case n < 25:
-      episodeSelection = 4
-      break
-    case n < 50:
-      episodeSelection = 5
-      break
-    case n < 75:
-      episodeSelection = 10
-      break
-    default:
-      episodeSelection = getRandomArbitrary(1, numberOfEpisodes)
-  }
-}
-
-if (seasonSelection === 8) {
-  var x = Math.floor(Math.random() * 100)
-  switch (x) {
-    case x < 50:
-      episodeSelection = 4
-      break
-    default:
-      episodeSelection = getRandomArbitrary(1, numberOfEpisodes)
-  }
+  var numberOfEpisodes = 10
+  var episodeSelection = getRandomArbitrary(1, numberOfEpisodes)
 }
 
 document.getElementById('episode').innerHTML = episodeSelection
